85068255403557120dd813c50bfc117eedc50274
João Almeida <jvastos@gmail.com>
2021-11-22 23:35:29 +0100

    man running

--- actions-bar.js
+++ actions-bar.js
@@ -15,24 +15,45 @@ function create () {
     player = this.add.sprite(350, 280, 'man');
     player.setScale(0.4);
 
-    actionBar = this.add.graphics();
-    actionBar.fillStyle(0x15702b, 1);
-    actionBar.fillRoundedRect(200, 330, 300, 70, { tl: 30, tr: 30, bl: 0, br: 0 });
+    timeline = this.tweens.timeline({
 
-    circleAction1 = this.add.circle(250, 365, 25, 0xffffff).setInteractive({ cursor: 'pointer' });
-    circleAction1.setStrokeStyle(2, 0x000000).setInteractive({ cursor: 'pointer' });
-    circleAction2 = this.add.circle(350, 365, 25, 0xffffff).setInteractive({ cursor: 'pointer' });
-    circleAction2.setStrokeStyle(2, 0x000000).setInteractive({ cursor: 'pointer' });
-    circleAction3 = this.add.circle(450, 365, 25, 0xffffff).setInteractive({ cursor: 'pointer' });
-    circleAction3.setStrokeStyle(2, 0x000000).setInteractive({ cursor: 'pointer' });
+        targets: player,
+        loop: Infinity,
+        yoyo: true,
+        tweens: [
+            {
+            y: 270,
+            duration: 200,
+            ease: 'power2',
+            loop: 10,
+            yoyo: true
+            },
+            {
+                y: 270,
+                duration: 400,
+                ease: 'power2',
+                loop: 1,
+                yoyo: true
+            }
+        ]
+    },
 
+    actionBar = this.add.graphics(),
+    actionBar.fillStyle(0x15702b, 1),
+    actionBar.fillRoundedRect(200, 330, 300, 70, { tl: 30, tr: 30, bl: 0, br: 0 }),
+
+    circleAction1 = this.add.circle(250, 365, 25, 0xffffff).setInteractive({ cursor: 'pointer' }),
+    circleAction1.setStrokeStyle(2, 0x000000).setInteractive({ cursor: 'pointer' }),
+    circleAction2 = this.add.circle(350, 365, 25, 0xffffff).setInteractive({ cursor: 'pointer' }),
+    circleAction2.setStrokeStyle(2, 0x000000).setInteractive({ cursor: 'pointer' }),
+    circleAction3 = this.add.circle(450, 365, 25, 0xffffff).setInteractive({ cursor: 'pointer' }),
+    circleAction3.setStrokeStyle(2, 0x000000).setInteractive({ cursor: 'pointer' })
+    );
 }
 
 function update() {
 
 }
-
-function playerRunning () {}
 
 
 
