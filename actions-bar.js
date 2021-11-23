e7348da63831822b9cf5c3513eeac0607bf8eb01
João Almeida <jvastos@gmail.com>
2021-11-23 15:10:39 +0100

    actions popping from bar

--- actions-bar.js
+++ actions-bar.js
@@ -1,19 +1,77 @@
-let background, player, actionBar, circleAction1, circleAction2, circleAction3;
+let background, player, actionBar, circleAction1, circleAction2, circleAction3, net, trash, mouth, timeline;
 
 
 function preload() {
     this.load.image('background', './assets/images/sunsetintheswamp.png');
     this.load.image('man', './assets/images/man.png');
+    this.load.image('net', './assets/images/net3.png');
+    this.load.image('trash', './assets/images/trash-can.png');
+    this.load.image('mouth', './assets/images/mouth.png');
 }
 
 function create () {
 
-    background = this.add.image(0, 0, 'background');
-    background.setOrigin (0, 0);
+    // ----- CREATING ELEMENTS -----
+
+    background = this.add.image(config.width/2, config.height/2, 'background');
     background.setScale(0.7);
 
     player = this.add.sprite(350, 280, 'man');
     player.setScale(0.4);
+
+    actionBar = this.add.graphics();
+    actionBar.fillStyle(0x15702b, 1);
+    actionBar.fillRoundedRect(200, 330, 300, 70, { tl: 30, tr: 30, bl: 0, br: 0 });
+
+    circleAction1 = this.add.circle(250, 365, 25, 0xffffff).setInteractive({ cursor: 'pointer' });
+    circleAction1.setStrokeStyle(2, 0x000000);
+    circleAction2 = this.add.circle(350, 365, 25, 0xffffff).setInteractive({ cursor: 'pointer' });
+    circleAction2.setStrokeStyle(2, 0x000000);
+    circleAction3 = this.add.circle(450, 365, 25, 0xffffff).setInteractive({ cursor: 'pointer' });
+    circleAction3.setStrokeStyle(2, 0x000000);
+
+    net = this.add.image(250, 366, 'net').setInteractive();
+    net.setScale(0.07);
+    net.on("pointerdown", function() {
+        net.y = 300;
+        net.setScale(1.3);
+        net.setAlpha(0.5);
+    });
+    net.on("pointerup", function() {
+        net.y = 366;
+        net.setScale(0.07);
+        net.setAlpha(1);
+    });
+
+    trash = this.add.image(350, 364, 'trash').setInteractive();
+    trash.setScale(0.04);
+    trash.on("pointerdown", function() {
+        trash.y = 300;
+        trash.setScale(0.8);
+        trash.setAlpha(0.5);
+    });
+    trash.on("pointerup", function() {
+        trash.y = 364;
+        trash.setScale(0.04);
+        trash.setAlpha(1);
+    });
+
+    mouth = this.add.image(450, 364, 'mouth').setInteractive();
+    mouth.setScale(0.04);
+    mouth.on("pointerdown", function() {
+        mouth.y = 300;
+        mouth.setScale(0.8);
+        mouth.setAlpha(0.5);
+    });
+    mouth.on("pointerup", function() {
+        mouth.y = 364;
+        mouth.setScale(0.04);
+        mouth.setAlpha(1);
+    });
+
+    // ----- END OF CREATING ELEMENTS -----
+
+    // ----- TIMELINE WITH TWEEN TO MAKE PLAYER RUN -----
 
     timeline = this.tweens.timeline({
 
@@ -36,25 +94,68 @@ function create () {
                 yoyo: true
             }
         ]
-    },
+    });
 
-    actionBar = this.add.graphics(),
-    actionBar.fillStyle(0x15702b, 1),
-    actionBar.fillRoundedRect(200, 330, 300, 70, { tl: 30, tr: 30, bl: 0, br: 0 }),
+    // ----- END OF TIMELINE WITH TWEEN TO MAKE PLAYER RUN -----
 
-    circleAction1 = this.add.circle(250, 365, 25, 0xffffff).setInteractive({ cursor: 'pointer' }),
-    circleAction1.setStrokeStyle(2, 0x000000).setInteractive({ cursor: 'pointer' }),
-    circleAction2 = this.add.circle(350, 365, 25, 0xffffff).setInteractive({ cursor: 'pointer' }),
-    circleAction2.setStrokeStyle(2, 0x000000).setInteractive({ cursor: 'pointer' }),
-    circleAction3 = this.add.circle(450, 365, 25, 0xffffff).setInteractive({ cursor: 'pointer' }),
-    circleAction3.setStrokeStyle(2, 0x000000).setInteractive({ cursor: 'pointer' })
-    );
+    /* // ----- CAMERA ZOOM OUT TO BACKROUND -----
+    const camera = this.cameras.main;
+      
+    camera.setZoom(2);
+    camera.ignore([player, actionBar, circleAction1, circleAction2, circleAction3, net, trash, mouth]);
+      
+    const tweenConfig = {
+        targets: camera,
+        zoom: 1,
+        duration: 50000,
+        ease: 'Sine.easeInOut'
+      }
+      
+      this.tweens.add(tweenConfig);
+
+    // ----- END OF CAMERA ZOOM OUT TO BACKROUND ----- */
 }
 
 function update() {
-
+    
 }
 
+// ----- MAKING THE WORLD RESPONSIVE -----
+
+/*
+function resize() {
+    var canvas = document.querySelector("canvas");
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var windowRatio = windowWidth / windowHeight;
+    var gameRatio = game.config.width / game.config.height;
+    if(windowRatio < gameRatio){
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    }
+    else{
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
+    }
+}
+
+ window.onload = function(){
+    var gameConfig = {
+        type: Phaser.AUTO,
+        width: gameOptions.gameWidth,
+        height: gameOptions.gameHeight,
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+    var game = new Phaser.Game(gameConfig);
+    resize();
+    window.addEventListener("resize", resize, false);
+} */
+
+// ----- END OF MAKING THE WORLD RESPONSIVE -----
 
 
 const config = {
