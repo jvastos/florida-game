d5e31644196acaedfb12d205e55896a97f3f043a
João Almeida <jvastos@gmail.com>
2021-11-29 18:39:45 +0100

    different threats comming at different increasing speed

--- main.js
+++ main.js
@@ -20,26 +20,16 @@ randomX,
 addThreat,
 threat,
 score = 0,
+scoreDisplay,
 destroyListener,
 randomNumber,
 randomThreat,
+speed = 0.8,
 ifAddThreat = true,
-initialYPosition = 80;
-
-/* function createRandomThreat () {
-    let randomNumber = Math.floor(Math.random() * 5);
-    if (randomNumber === 0) {
-        randomThreat === 'alligator'
-    } else if (randomNumber === 1) {
-        randomThreat === 'cuban'
-    } else if (randomNumber === 2) {
-        randomThreat === 'islandboys'
-    } else if (randomNumber === 3) {
-        randomThreat === 'snake'
-    } else if (randomNumber === 4) {
-        randomThreat === 'trump'
-    }
- } */
+initialYPosition = 80,
+theTimer,
+scaleSpeed = 0.003,
+delay = 1000;
 
 class Main extends Phaser.Scene {
     constructor () {
@@ -65,12 +55,15 @@ class Main extends Phaser.Scene {
 
     create () {
 
+    theTimer = setInterval(() => {speed += 0.6; scaleSpeed += 0.006}, 10000);
+
     // ----- CREATING ELEMENTS -----
 
     background = this.add.image(config.width/2, config.height/2, 'background');
     background.setScale(0.7);
 
-    // this.createThreat('alligator');
+    scoreDisplay = this.add.text(20, 20, "You have survived to: " + "0" + " Florida threats", {fontSize: "25px"})
+
     this.createThreat();
 
     player = this.add.sprite(350, 280, 'man');
@@ -110,105 +103,128 @@ class Main extends Phaser.Scene {
             net.setAlpha(1);
         });
 
-        trash = this.add.image(350, 364, 'trash').setInteractive();
+    trash = this.add.image(350, 364, 'trash').setInteractive();
+    trash.setScale(0.04);
+    trash.on("pointerdown", function() {
+        metalSound.play();
+        trash.y = 300;
+        trash.setScale(0.8);
+        trash.setAlpha(0.5);
+        trump.destroy();
+        addThreat = "trump";
+        snake.destroy();
+        addThreat = "snake";
+        islandboys.destroy();
+        addThreat = "islandboys";
+    });
+    trash.on("pointerup", function() {
+        trash.y = 364;
         trash.setScale(0.04);
-        trash.on("pointerdown", function() {
-            metalSound.play();
-            trash.y = 300;
-            trash.setScale(0.8);
-            trash.setAlpha(0.5);
-        });
-        trash.on("pointerup", function() {
-            trash.y = 364;
-            trash.setScale(0.04);
-            trash.setAlpha(1);
-        });
+        trash.setAlpha(1);
+    });
 
-        mouth = this.add.image(450, 364, 'mouth').setInteractive();
+    mouth = this.add.image(450, 364, 'mouth').setInteractive();
+    mouth.setScale(0.04);
+    mouth.on("pointerdown", function() {
+        biteSound.play();
+        mouth.y = 300;
+        mouth.setScale(0.8);
+        mouth.setAlpha(0.5);
+        cuban.destroy();
+        addThreat = "cuban";
+    });
+
+    mouth.on("pointerup", function() {
+        mouth.y = 364;
         mouth.setScale(0.04);
-        mouth.on("pointerdown", function() {
-            biteSound.play();
-            mouth.y = 300;
-            mouth.setScale(0.8);
-            mouth.setAlpha(0.5);
-            /* cuban.destroy();
-            addThreat = "cuban"; */
-        });
-        mouth.on("pointerup", function() {
-            mouth.y = 364;
-            mouth.setScale(0.04);
-            mouth.setAlpha(1);
-        });
+        mouth.setAlpha(1);
+    });
 
     // ----- END OF CREATE CLICKS FOR THE ACTIONS -----
 
     // ----- CREATE KEY BIDING FOR 1,2 AND 3 TO FIRE ACTIONS -----
 
-        this.input.keyboard.on('keydown', function (e) {
-            if (e.key == "1") {
-                throwSound.play();
-                net.y = 300;
-                net.setScale(1.3);
-                net.setAlpha(0.5);
-                alligator.destroy();
-                addThreat = "alligator";
-            } else if (e.key == "2") {
-                metalSound.play();
-                trash.y = 300;
-                trash.setScale(0.8);
-                trash.setAlpha(0.5);
-            } else if (e.key == "3") {
-                biteSound.play();
-                mouth.y = 300;
-                mouth.setScale(0.8);
-                mouth.setAlpha(0.5);
-                cuban.destroy();
-                addThreat = "cuban";
-            }
-        }, this);
+    this.input.keyboard.on('keydown', function (e) {
 
-        this.input.keyboard.on('keyup', function (e) {
-            if (e.key == "1") {
-                net.y = 366;
-                net.setScale(0.07);
-                net.setAlpha(1);
-            } else if (e.key == "2") {
-                trash.y = 364;1
-                trash.setScale(0.04);
-                trash.setAlpha(1);
-            } else if (e.key == "3") {
-                mouth.y = 364;
-                mouth.setScale(0.04);
-                mouth.setAlpha(1);
+        if (e.key == "1") {
+            throwSound.play();
+            net.y = 300;
+            net.setScale(1.3);
+            net.setAlpha(0.5);
+            alligator.destroy();
+            addThreat = "alligator";
+
+        } else if (e.key == "2") {
+            metalSound.play();
+            trash.y = 300;
+            trash.setScale(0.8);
+            trash.setAlpha(0.5);
+
+            if(threat===snake) {
+                snake.destroy();
+                addThreat = "snake";
+
+            } else if (threat===trump) {
+                trump.destroy();
+                addThreat = "trump";
+
+            } else if (threat===islandboys) {
+                islandboys.destroy();
+                addThreat = "islandboys";
             }
-        }, this);
+        
+        } else if (e.key == "3") {
+            biteSound.play();
+            mouth.y = 300;
+            mouth.setScale(0.8);
+            mouth.setAlpha(0.5);
+            cuban.destroy();
+            addThreat = "cuban";
+        }
+    }, this);
+
+    this.input.keyboard.on('keyup', function (e) {
+        if (e.key == "1") {
+            net.y = 366;
+            net.setScale(0.07);
+            net.setAlpha(1);
+        } else if (e.key == "2") {
+            trash.y = 364;1
+            trash.setScale(0.04);
+            trash.setAlpha(1);
+        } else if (e.key == "3") {
+            mouth.y = 364;
+            mouth.setScale(0.04);
+            mouth.setAlpha(1);
+        }
+    }, this);
         
     // ----- END OF CREATE KEY BIDING FOR 1,2 1AND 3 TO FIRE ACTIONS -----
 
     // ----- TWEEN FOR THE PLAYER RUNNIG -----
 
-        timeline = this.tweens.timeline({
+    timeline = this.tweens.timeline({
 
-            targets: player,
-            loop: Infinity,
-            yoyo: true,
-            tweens: [
-                {
-                    y: 270,
-                    duration: 200,
-                    ease: 'power2',
-                    loop: 10,
-                    yoyo: true
-                },
-                {
-                    y: 275,
-                    duration: 300,
-                    ease: 'power2',
-                    loop: 4,
-                    yoyo: true
-                }
-            ]
-        });
+        targets: player,
+        loop: Infinity,
+        yoyo: true,
+        tweens: [
+            {
+                y: 270,
+                duration: 200,
+                ease: 'power2',
+                loop: 10,
+                yoyo: true
+            },
+            {
+                y: 275,
+                duration: 300,
+                ease: 'power2',
+                loop: 4,
+                yoyo: true
+            }
+        ]
+    });
 
     }
 
@@ -222,19 +238,16 @@ class Main extends Phaser.Scene {
         if (backgroundScale > 1 ) {
             backgroundScale = backgroundScale - 0.0002;
             background.setScale(backgroundScale);
-        }
-
-        // this.moveThreat(alligator, 0.8);
-        // this.moveThreat(cuban, 0.8);
-        // this.moveThreat(snake, 0.8);
-        // this.moveThreat(islandboys, 0.8);
-        // this.moveThreat(trump, 0.8);
+        };
 
         if(ifAddThreat) {
             this.createThreat();
         } else {
-            this.moveThreat(threat, 0.8)
-        }
+            this.moveThreat(threat)
+        };
+        
+        //to stop the timer
+        if(speed > 5)clearInterval(theTimer);
     }
 
     createThreat() {
@@ -273,14 +286,19 @@ class Main extends Phaser.Scene {
 
         ifAddThreat = false;
         scale = 0;
+
+
     }
 
+
     
-    moveThreat(threat, speed) {
+    // TIMER INSIDE CREATE FUNCTION TO BOTH CALL NEW DANGER AND
+    // 
+    moveThreat(threat) {
             
             threat.y += speed;
 
-            scale = scale + 0.003
+            scale = scale + scaleSpeed
             threat.setScale(scale)
 
             if(threat.y > config.height) {
@@ -293,18 +311,17 @@ class Main extends Phaser.Scene {
 
     resetThreat(threat) {
         threat.destroy()
-            // threat.y = initialYPosition + (backgroundScale / 2) * -1;
-            // scale = 0;
-
-            // let randomX = Phaser.Math.Between(0, config.width);
-            // threat.x = randomX;
         }
 
     addToScore() {
         if(threat.y < config.height) {
             score++;
+            scoreDisplay.setText("You have survived to: " + score + " Florida threats");
         }
        console.log('score', score)
     }
+
+    
 }
+
 
