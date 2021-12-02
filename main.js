df744e9e3f2baa27130e775a2d3493e182feedf3
João Almeida <jvastos@gmail.com>
2021-12-02 14:36:15 +0100

    new font

--- main.js
+++ main.js
@@ -97,13 +97,13 @@ class Main extends Phaser.Scene {
         background.setScale(0.7);
 
         scoreDisplay = this.add.text(20, 20, "Threats conquered: " + score, {
-            fontFamily: 'IM Fell French Canon SC',
+            fontFamily: 'Kanit',
             fontSize: "40px"
         });
         scoreDisplay.setTint(0x20b344, 0x20b378, 0x20b3a7, 0x2031b3);
 
         livesDisplay = this.add.text(20, 70, "Lives: " + lives, {
-            fontFamily: 'IM Fell French Canon SC',
+            fontFamily: 'Kanit',
             fontSize: "40px"
         });
         livesDisplay.setTint(0x20b344, 0x20b378, 0x20b3a7, 0x2031b3);
@@ -382,48 +382,48 @@ class Main extends Phaser.Scene {
         this.scene.pause();
 
         gameOverText = this.add.text(window.innerWidth / 2, window.innerHeight / 2 - 50, "You survived " + score + " threats", {
-            fontFamily: 'IM Fell French Canon SC',
+            fontFamily: 'Kanit',
             fontSize: "30px"
         })
         gameOverText.setOrigin(0.5);
 
         gameOverText2 = this.add.text(window.innerWidth / 2, window.innerHeight / 2 + 100, "Wait some seconds and you can try again", {
-            fontFamily: 'IM Fell French Canon SC',
+            fontFamily: 'Kanit',
             fontSize: "20px"
         })
         gameOverText2.setOrigin(0.5);
 
         if (score < 10) {
             looseMsg1 = this.add.text(window.innerWidth / 2, window.innerHeight / 2, "Would you even survive a Christmas dinner?", {
-                fontFamily: 'IM Fell French Canon SC',
+                fontFamily: 'Kanit',
                 fontSize: "30px"
             });
             looseMsg1.setOrigin(0.5);
     
         } else if (score > 10) {
             looseMsg2 = this.add.text(window.innerWidth / 2, window.innerHeight / 2, "Nice try! But you’re still gator bait", {
-                fontFamily: 'IM Fell French Canon SC',
+                fontFamily: 'Kanit',
                 fontSize: "30px"
             });
             looseMsg2.setOrigin(0.5);
 
         } else if (score > 30) {
             looseMsg3 = this.add.text(window.innerWidth / 200, window.innerHeight / 2, "Dale! You are a natural.", {
-                fontFamily: 'IM Fell French Canon SC',
+                fontFamily: 'Kanit',
                 fontSize: "30px"
             });
             looseMsg3.setOrigin(0.5);
 
         } else if (score > 40) {
             looseMsg4 = this.add.text(window.innerWidth / 2, window.innerHeight / 2, "You are the new Mr. 305, Mr. Worldwide!", {
-                fontFamily: 'IM Fell French Canon SC',
+                fontFamily: 'Kanit',
                 fontSize: "30px"
             });
             looseMsg4.setOrigin(0.5);
 
         }else if (score > 50) {
             looseMsg5 = this.add.text(window.innerWidth / 2, window.innerHeight / 2, "Wow. You are now officially a honorary citizen of the great state of Florida, amigo", {
-                fontFamily: 'IM Fell French Canon SC',
+                fontFamily: 'Kanit',
                 fontSize: "30px"
         });
             looseMsg5.setOrigin(0.5);
